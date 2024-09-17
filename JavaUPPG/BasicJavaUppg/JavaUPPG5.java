ed1a94849009f72ceb925efd221066df2c5878bb
Wille4321 <wille.nordstrom08@gmail.com>
2024-09-17 11:34:07 +0300

    Add files via upload

--- JavaUPPG/BasicJavaUppg/JavaUPPG5.java
+++ JavaUPPG/BasicJavaUppg/JavaUPPG5.java
@@ -1,29 +1,24 @@
 
-public class JavaBetygUPPG {
+
+public class JavaUPPG5 {
 
     
     public static void main(String args[]) {
-        int Score = 100;  
-        if (Score <= 95);
-            System.out.print("5"); {
-    
-    }if (Score <= 89);
-    else if(Score <= 82); {
-            System.out.print("4");
-    }if (Score <= 79);        
-    else if (Score <= 76); {
-            System.out.print("3");
-    }if (Score <= 69);        
-    else if (Score <= 61); {
-            System.out.print("2");
-    }if (Score <= 59);        
-    else if (Score <= 30); {
-            System.out.print("1");
-    }if (Score <= 24);        
-    else if (Score <= 5); {
-            System.out.print("0");
-            
-        }
-            
-    }
-}
+        int Score = 50;  
+            if (Score >= 90){
+                System.out.print("5");
+            }else if (Score >= 80) {
+                System.out.print("4"); 
+            }else if (Score >= 70) {
+                System.out.print("3"); 
+            }else if (Score >= 60) {
+                System.out.print("2"); 
+            }else if (Score >= 25) {
+                System.out.print("1"); 
+            }
+            else{
+                 System.out.print("0"); 
+            }
+     }   
+ }  
+
