5a8c2d16ed4a500cf83c7c05b6fbf9de194529e8
Wille4321 <wille.nordstrom08@gmail.com>
2024-09-18 09:00:46 +0300

    Add files via upload

--- JavaUPPG/BasicJavaUppg/JavaUPPG7.java
+++ JavaUPPG/BasicJavaUppg/JavaUPPG7.java
@@ -1,30 +1,24 @@
-
 public class JavaUPPG7 {
 
     public static void main(String args[]) {
-        //Arrey utan Loop
-        /*
         int[] Num = {10, 20, 30, 40, 50};
-            System.out.println(Num[0]);
-            
-            System.out.println(Num[4]);
-        */
         
-        //Arrey med Loop
-        /*
-        String[] Num = {"10", "20", "30", "40", "50"};
-        for (String Num1 : Num) {
-            System.out.println(Num1);
-        }  
-        */
-        //Arrey med split
-        /*
-        String myNum = "10 20 30, 40 50";
-        String Num = "[,\\.\\a]";
-        String[] myArray = myNum.split(Num);
-            for (String a : myArray) {
-                System.out.println(a);
-        }
-        */
+        //Alla siffror
+            for (int Num1 : Num) {
+                System.out.println(Num1);
+        } 
+        //Första och sista siffran
+            System.out.println(Num[0]);
+            System.out.println(Num[4]);
+        //Sum och delat
+            int Sum = 0;
+            int i;
+            int Sum2 = 2;
+              for (i = 0; i < Num.length; i++) {
+                  Sum += Num[i];
+              }
+                    System.out.println("The sum is: " + Sum);
+                        System.out.println(Sum / Sum2);
+            
     }
 }
